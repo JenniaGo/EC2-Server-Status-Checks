a6e6b6c4703eb2aaae54a16ef2f3c5a5732bb2f0
JenniaGo <jenniagorelik@gmail.com>
2022-12-12 21:16:02 +0200

    Schedule Status Checks every 5 seconds

---
+++ Schedule_Status_Checks.py
@@ -0,0 +1,21 @@
+import boto3
+import schedule
+
+# Chose a resource \ client
+ec2_client = boto3.client("ec2", region_name="us-east-1")
+
+
+# call check status on instance and system
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status()
+    for status in statuses['InstanceStatuses']:
+        ins_status = status['InstanceStatus']['Status']
+        sys_status = status['SystemStatus']['Status']
+        state = status['InstanceState']
+        print(f"Instance ID {status['InstanceId']} is {state['Name']} with instance status {ins_status} and system status is {sys_status}")
+# run every 5 seconds
+schedule.every(5).seconds.do(check_instance_status)
+# examples: schedule.every().monday.at("12:00") or schedule.every().day.at("1:00") or schedule.every().hour
+
+while True:
+    schedule.run_pending()
